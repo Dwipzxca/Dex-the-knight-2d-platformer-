f7ac4a0e7eca683cb298712ebee26429d25b39ea
Dwipzx <dwipsomzxben10@gmail.com>
2026-06-06 10:22:51 +0530

    Dex the knight

---
+++ Scripts/player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+
+	#direction maybe be 1 , 0 or -1 according to move ldft or right
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+		
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+		
+	
+	#animations
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("running")
+	else:
+		animated_sprite_2d.play("jumping")
+	
+	
+	
+	
+	if direction:
+		velocity.x = direction * SPEED
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+
+	move_and_slide()
