e98d83196bf51ab254a5ba680ce2c89e4ee9c86a
Dwipzx <dwipsomzxben10@gmail.com>
2026-06-10 14:01:15 +0530

    added double jump

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-
+var jumpcount = 1
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -250.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jumpsound: AudioStreamPlayer = $jumpsound
 @onready var enemydeathsound: AudioStreamPlayer = $enemydeathsound
@@ -18,9 +18,15 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		jumpcount = 0
+		
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jumpcount<2:
+		
+		jumpcount += 1
+		
 		velocity.y = JUMP_VELOCITY
 		jumpsound.play()
 		
